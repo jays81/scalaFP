e70499c12febdf23f80410e8022861da675dcffc
jay <jays81@yahoo.com>
2016-01-04 16:46:11 +0000

    add fiboancci tail rec

--- scalaFP/src/chapter2/MathModule.scala
+++ scalaFP/src/chapter2/MathModule.scala
@@ -11,8 +11,24 @@ object MathModule {
     msg.format(x, abs(x))
   }
   
+  def fibonacci(n:Int):Int = {
+    def go(n:Int, previous: Int, current:Int):Int = n match {
+      case 0  => previous
+      case _  => go(n-1, current, current + previous)
+     
+    }
+     go(n, 0, 1)
+  }
+  
+  private def formatFibonacci(x:Int) = {
+    val msg = "The %dth fibonnaci number is %d"
+    msg.format(x, fibonacci(x))
+  }
+  
   def main(args:Array[String]):Unit = {
     println(formatAbs(-42))
+    println(formatFibonacci(10))
+    
   }
   
 }
